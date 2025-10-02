0db79bcc43c9672bc21f793f0b9b64246e4c2150
nj442442-collab <nj442442@gmail.com>
2025-10-02 08:26:28 -0500

    setup player variable and output

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Enter player name (or "done"): Alice
+Enter score for Alice: 95
+Enter player name (or "done"): Bob
+Enter score for Bob: notanumber
+Invalid score. Skipping this entry.
+Enter player name (or "done"): Cara
+Enter score for Cara: 88
+Enter player name (or "done"): done
+"""
+#store players as a list 
+player=[]
+
+
+#output player summary
+print("===summary===")
+print(f"Players: {None}")
+print(f"Highest: {None}")
+print(f"Lowest: {None}")
+print(f"Average: {None}")
