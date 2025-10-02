f906c173d694e996c6504c73e01bcfd76235c401
nj442442-collab <nj442442@gmail.com>
2025-10-02 09:02:05 -0500

    implement average score

--- main.py
+++ main.py
@@ -9,16 +9,34 @@ Enter score for Cara: 88
 Enter player name (or "done"): done
 """
 #store players as a list 
-player=[("Sally",95),("Tobi",88),("Sandeep",10),("Zainab",5)]
+players=[("Sally",95),("Tobi",88),("Sandeep",10),("Zainab",5)]
 
 # setup variable to store summary values
 highest_score_player = (None,0)
 lowest_score_player = (None,0)
 average_score = 0
 
+##pull the scores into a separate list
+#declare a new variable to hold a list of scores
+#integrate over the player list
+#add each score to the new list
+player_scores = []
+score_total = 0
+for player in players:
+    player_scores.append(player[1])
+    score_total += player[1]
+average_score = score_total / len(player_scores)
+print(score_total)
+print(player_scores)
+
+#find the average
+#add all the scores together 
+#divide by the number of scores
+#set the value of average_score
+
 #output player summary
 print("===summary===")
 print(f"Players: {len(player)}")
 print(f"Highest: {None}") # Sally - 95
 print(f"Lowest: {None}") # Zainab - 5
-print(f"Average: {None}")
+print(f"Average: {average_score}")
